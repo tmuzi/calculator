75faa49221943869c42da72f22768ee5a0ed3797
taf <muzirat0@gmail.com>
2026-01-30 23:15:37 +0200

    input events

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
+// g++ main.cpp `pkg-config --cflags --libs sdl3`
+
 #include <iostream>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 #include <string>
 
+bool init();
+bool key_events();
+
 /* Constants */
 //Screen dimension constants
-constexpr int kScreenWidth{ 640 };
-constexpr int kScreenHeight{ 480 };
+constexpr int kScreenHeight{ 518 };
+constexpr int kScreenWidth{ 320 };
 
 
 /* Global Variables */
@@ -15,25 +20,46 @@ SDL_Window* gWindow{ nullptr };
 
 int main()
 {
+	if (!init())
+		return 1;
+
+	while (key_events());
+
+	SDL_Quit();
+
+	return 0;
+}
+
+bool init()
+{
 
 
 	if (SDL_Init(SDL_INIT_VIDEO) == false)
 	{
 		SDL_Log("SDL could not initialize! SDL error: %s\n", SDL_GetError());
 
-		return 1;
+		return false;
 	}
 	
 	if(gWindow = SDL_CreateWindow("Hello SDL3", kScreenWidth, kScreenHeight, 0); gWindow == nullptr)
 	{
-		SDL_Log( "Window could not be created! SDL error: %s\n", SDL_GetError() );
+		SDL_Log("Window could not be created! SDL error: %s\n", SDL_GetError());
 
-		return 1;
+		return false;
 	}
 
-	SDL_Delay(10000);
+	return true;
+}
 
-	SDL_Quit();
+bool key_events()
+{
+	SDL_Event e;
 
-	return 0;
+	while (SDL_PollEvent(&e))
+	{
+		switch (e.type)
+			case SDL_EVENT_QUIT:
+				return false;
+	}
+	return true;
 }
